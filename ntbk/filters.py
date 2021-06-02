f6edaf9e6fe0d24b110795a8a83fc19461c3998d
pcaynzec <toto.palacios.a@gmail.com>
2021-06-02 13:14:04 -0400

    slider update

--- ntbk/filters.py
+++ ntbk/filters.py
@@ -1,5 +1,5 @@
 from django.db.models.fields import BooleanField
-from django.forms.widgets import Select
+from django.forms.widgets import Input, Select
 import django_filters
 from django_filters import widgets
 from django_filters.filters import BooleanFilter, CharFilter , AllValuesFilter, NumberFilter , RangeFilter
@@ -21,13 +21,16 @@ def multiple_search(queryset, name, value):
 
 class NtbkFilter(django_filters.FilterSet):
 
-    q = CharFilter(method=multiple_search)
+    q = CharFilter(method=multiple_search, widget=Input(attrs={ 'type' : 'search',
+                                                                'placeholder' : 'mm food...',
+                                                                'autocomplete' : 'off',
+                                                               }))
     g = CharFilter(field_name='gpuDedi', lookup_expr='icontains')
     #ramint = CharFilter(field_name='ram', lookup_expr='icontains')
-    ramint= AllValuesFilter(field_name='ramint', widget=Select(attrs={ 'class' : 'filter'}))
-    nombre = q
+    ramint = AllValuesFilter(field_name='ramint', widget=Select(attrs={ 'class' : 'filter'}))
+    precioRange = RangeFilter(field_name='precioint')
 
     class Meta:
         model = Note
-        fields = ['precio']
+        fields = ['precioint']
         
